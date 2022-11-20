24d7d6d156429c832f50462457577bd034afb264
edsonfe92 <ev.fernandez.2019@alumnos.urjc.es>
2022-11-20 22:20:06 +0100

    Warning message

--- Assets/Scripts/BattleLogic/BattleButton.cs
+++ Assets/Scripts/BattleLogic/BattleButton.cs
@@ -8,17 +8,30 @@ public class BattleButton : MonoBehaviour
 {
     public Enemy enemy;
     private MapPathSelector mapController;
+    public GameObject dialogueBoxPrefab;
+    public Transform pivot;
+    public DialogueObject warning;
     void Start()
     {
+        pivot = GameObject.Find("WarningTrigger").transform;
         if(SceneManager.GetActiveScene().name != "DebugScene") mapController = GameObject.FindGameObjectWithTag("MapController").GetComponent<MapPathSelector>();
         gameObject.GetComponent<Button>()?.onClick.AddListener(() => ToBattleScene(enemy));
     }
 
     public void ToBattleScene(Enemy enemy)
     {
-        if (GameManager.playerData.playerDeck.Count < 5) return;
+        if (GameManager.playerData.playerDeck.Count < 5) {
+            NotEnoughCards(warning);
+            return;
+        }
         if (SceneManager.GetActiveScene().name != "DebugScene") GameManager.currentNode = mapController.GetGoIndex(gameObject);
         GameManager.nextEnemy = enemy;
         SceneManager.LoadScene("BattleScene");
+    }
+
+    public void NotEnoughCards(DialogueObject warningText) {
+        var obj = Instantiate(dialogueBoxPrefab,pivot);
+        obj.transform.localScale = obj.transform.localScale * 2;
+        obj.GetComponent<GenericDialogueBox>().dialogue = warningText;
     }
 }
